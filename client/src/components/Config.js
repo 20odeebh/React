92ea819b2085158d5419a5a7179e456344d15993
20odeebh <20odeebh@gmail.com>
2020-08-19 20:38:24 +0900

    리액트 클론코딩

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '15cd5ba4a649e53e49b85e6060be190a';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+
---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY } from '../../Config';
+
+
+function LandingPage() {
+
+    useEffect(() => {
+        const endpoint = '${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1';
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => console.log(response))
+    }, [])
+    return (
+        <div style={{width: '100%', margin: '0'}}>
+            {/*Main Image*/}
+
+            <div style={{width: '85%', margin: '1rem auto'}}>
+
+                <h2>Movies by latest</h2>
+                <hr />
+                {/* Movie Grid Cards */}
+
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center'}}>
+                <button>Load More</button>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
